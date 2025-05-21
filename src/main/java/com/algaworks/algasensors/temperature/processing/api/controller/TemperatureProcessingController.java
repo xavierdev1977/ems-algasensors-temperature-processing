3aeb2a288f7211b5cfa88cbcadc884b2a2ad9369
xavierdev1977 <xavier.rodrigo.fabiano@gmail.com>
2025-05-21 08:21:03 -0300

    Ajuste de algumas propriedades

---
+++ src/main/java/com/algaworks/algasensors/temperature/processing/api/controller/TemperatureProcessingController.java
@@ -0,0 +1,44 @@
+package com.algaworks.algasensors.temperature.processing.api.controller;
+
+import com.algaworks.algasensors.temperature.processing.api.model.TemperatureLogOutput;
+import com.algaworks.algasensors.temperature.processing.common.IdGenerator;
+import io.hypersistence.tsid.TSID;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.OffsetDateTime;
+
+@RestController
+@RequestMapping("/api/sensors/{sensorId}/temperatures/data")
+@Slf4j
+public class TemperatureProcessingController {
+
+    @PostMapping(consumes = MediaType.TEXT_PLAIN_VALUE)
+    public void data(@PathVariable TSID sensorId, @RequestBody String input) {
+        if (input == null || input.isBlank()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        Double temperature;
+
+        try {
+            temperature = Double.parseDouble(input);
+        } catch (NumberFormatException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        TemperatureLogOutput logOutput = TemperatureLogOutput.builder()
+                .id(IdGenerator.generateTimeBasedUUID())
+                .sensorId(sensorId)
+                .value(temperature)
+                .registeredAt(OffsetDateTime.now())
+                .build();
+
+        log.info(logOutput.toString());
+
+    }
+
+}
---
+++ src/main/java/com/algaworks/algasensors/temperature/processing/common/IdGenerator.java
@@ -0,0 +1,18 @@
+package com.algaworks.algasensors.temperature.processing.common;
+
+import com.fasterxml.uuid.Generators;
+import com.fasterxml.uuid.impl.TimeBasedEpochRandomGenerator;
+
+import java.util.UUID;
+
+public class IdGenerator {
+    private static final TimeBasedEpochRandomGenerator timeBasedEpochRandomGenerator =
+            Generators.timeBasedEpochRandomGenerator();
+
+    private IdGenerator() {
+    }
+
+    public static UUID generateTimeBasedUUID() {
+        return timeBasedEpochRandomGenerator.generate();
+    }
+}
